bc8b7062ac44e9849aa8ff2ef075d76d75eb4eef
ablackcat04 <wuru87@gmail.com>
2024-04-08 17:32:43 +0800

    add register struct

--- codeGen.h
+++ codeGen.h
@@ -2,6 +2,7 @@
 #define __CODEGEN__
 
 #include "parser.h"
+#include <stdbool.h>
 
 // Evaluate the syntax tree
 extern int evaluateTree(BTNode *root);
@@ -9,5 +10,13 @@ extern int evaluateTree(BTNode *root);
 // Print the syntax tree in prefix
 extern void printPrefix(BTNode *root);
 
+typedef struct regi
+{
+    bool is_id;
+    char id[MAXLEN];
+    int value;
+} Register;
+
+extern void initRegister(void);
 
 #endif // __CODEGEN__
