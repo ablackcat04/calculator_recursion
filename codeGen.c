4304d31cfac6569d27d9fafbb43dc3c0ba81b679
ablackcat04 <wuru87@gmail.com>
2024-04-08 17:33:09 +0800

    add the initRegister() function

--- codeGen.c
+++ codeGen.c
@@ -3,6 +3,37 @@
 #include <string.h>
 #include "codeGen.h"
 
+Register r[8];
+
+void initRegister()
+{
+    r[0].is_id = true;
+    r[1].is_id = true;
+    r[2].is_id = true;
+    r[3].is_id = false;
+    r[4].is_id = false;
+    r[5].is_id = false;
+    r[6].is_id = false;
+    r[7].is_id = false;
+
+    r[0].value = getval("x");
+    r[1].value = getval("y");
+    r[2].value = getval("z");
+    r[3].value = 0;
+    r[4].value = 0;
+    r[5].value = 0;
+    r[6].value = 0;
+    r[7].value = 0;
+
+    strcpy(r[0].id, "x");
+    strcpy(r[1].id, "y");
+    strcpy(r[2].id, "z");
+
+    printf("MOV r0 [0]\n");
+    printf("MOV r1 [4]\n");
+    printf("MOV r2 [8]\n");
+}
+
 int evaluateTree(BTNode *root) {
     int retval = 0, lv = 0, rv = 0;
 
